fe37c928cd28c22138b46ad1a4fc81077cc295ed
EunKyung Park <90767413+Coster97@users.noreply.github.com>
2023-10-07 21:56:58 +0900

    Update 10819.java

--- BFBT/10819.java
+++ BFBT/10819.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/* 시간복잡도 : O(n!) */
+// 시간복잡도 : O(n!)
+// 메모리 : 14788kb
+// 시간 : 152ms
 
 public class B_10819 {
 
